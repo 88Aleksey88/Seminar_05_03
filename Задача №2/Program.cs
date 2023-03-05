3f8142358790e0842332ec42337f60907d4fc057
Aleksey <Sad_libra1988@mail.ru>
2023-03-05 21:56:43 +0700

    Solved Task №2

--- Задача №2/Program.cs
+++ Задача №2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+Console.Write($"Max = {b} ; Min = {a}");
+else 
+Console.Write($"Max = {a} ; Min = {b}");
